c7e2e6b2ad1ef825798b82b37519e4e9094f0209
ctln-d <caitlindu10@gmail.com>
2026-05-02 22:32:04 -0400

    create 'fine, i'll work' page

---
+++ header.js
@@ -0,0 +1,5 @@
+const headerHTML = `
+<div class="header" style="margin-bottom: 30px">
+    <h1>Do It Later!</h1>
+</div>`
+document.body.insertAdjacentHTML('afterbegin', headerHTML)
--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-const excuses = ["You're not in the perfect state of mind right now.",
+const excuseList = ["You're not in the perfect state of mind right now.",
     "You are strategically delaying things.", "You work better under pressure.",
     "If today is not the due date, then it's not the do date.",
     "It is a problem for future you, so live in the moment.",
-    "If you can do it tomorrow, why do it today?", "Laugh now cry later."]
+    "If you can do it tomorrow, why do it today?",
+    "You can deal with the consequences later.",
+    "You are waiting until you are older and wiser to do get it done.", "Haste makes waste.",
+    "Great things take time.", "The more your mind is rested the more productive it will be.",
+    "Your best ideas come with a little waiting."]
 
-const reason = document.getElementById("reasonBtn")
+const excuse = document.getElementById("excuse")
+const excuseBtn = document.getElementById("excuseBtn")
 
-reason.addEventListener("click", function () {
-    alert("Button was clicked!")
+excuseBtn.addEventListener("click", function () {
+    let x = Math.floor(Math.random() * excuseList.length)
+    excuse.innerHTML = excuseList[x]
 })
