7c87963af5cde411ce214504477f8c32b52e9e07
ctln-d <caitlindu10@gmail.com>
2026-05-02 21:00:33 -0400

    start js code and alter main html

---
+++ script.js
@@ -0,0 +1,11 @@
+const excuses = ["You're not in the perfect state of mind right now.",
+    "You are strategically delaying things.", "You work better under pressure.",
+    "If today is not the due date, then it's not the do date.",
+    "It is a problem for future you, so live in the moment.",
+    "If you can do it tomorrow, why do it today?", "Laugh now cry later."]
+
+const reason = document.getElementById("reasonBtn")
+
+reason.addEventListener("click", function () {
+    alert("Button was clicked!")
+})
